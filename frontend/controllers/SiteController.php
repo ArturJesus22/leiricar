2f7f82754b90f374aef4776eea0f7907958f3758
ArturJesus22 <2220866@my.ipleiria.pt>
2023-11-23 16:28:00 +0000

    - Layout Aplicado;

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -47,7 +47,7 @@ class SiteController extends Controller
             'verbs' => [
                 'class' => VerbFilter::class,
                 'actions' => [
-                    'logout' => ['post'],
+                    'logout' => ['get'],
                 ],
             ],
         ];
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,8 +16,25 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
+    <!-- Basic -->
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/favicon.png" type="">
+    <title>LeiriCar - Home</title>
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <!-- font awesome style -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -25,41 +42,34 @@ AppAsset::register($this);
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
 
-<header>
+<header class="header_section">
+    <div class="container">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => '<img width="250" src="images/logo.png" alt="#" />',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+            'class' => 'navbar navbar-expand-lg custom_nav-container',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Produtos', 'url' => ['/produto/']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-        ['label' => 'Cargo', 'url' => ['/site/teste']],
+        ['label' => 'Home |', 'url' => ['/produto/']],
+        ['label' => 'Produtos |', 'url' => ['/produto/']],
+        ['label' => 'Lavagens |', 'url' => ['/produto/']],
+        ['label' => 'Sobre Nós |', 'url' => ['/site/about']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/clientes/create']];
+        $menuItems[] = ['label' => 'Registar', 'url' => ['/clientes/create']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = ['label' => 'Logout', 'url' => ['/site/logout']];
     }
-
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+        'options' => ['class' => 'navbar-nav'],
         'items' => $menuItems,
     ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
-    NavBar::end();
     ?>
+    </div>
 </header>
 
 <main role="main" class="flex-shrink-0">
@@ -72,14 +82,51 @@ AppAsset::register($this);
     </div>
 </main>
 
-<footer class="footer mt-auto py-3 text-muted">
+<footer>
     <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="full">
+                    <div class="logo_footer">
+                        <a href="#"><img width="210" src="images/logo.png" alt="#" /></a>
+                    </div>
+                    <div class="information_f">
+                        <p><strong>Endereço:</strong> Estrada Nacional 113, Lote 21, Edificio A, Piso 1 Zona Industrial dos Pousos, 2410-205 Leiria</p>
+                        <p><strong>Telefone:</strong> +351 912 960 878</p>
+                        <p><strong>EMAIL:</strong> arturjesus324@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="widget_menu">
+                                    <h3>Páginas</h3>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Produtos</a></li>
+                                        <li><a href="#">Lavagens</a></li>
+                                        <li><a href="#">Sobre Nós</a></li>
+                                        <li><a href="#">Contactos</a></li>
+                                    </ul>
+                                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
 
 <?php $this->endBody() ?>
+<script src="js/jquery-3.4.1.min.js"></script>
+<!-- popper js -->
+<script src="js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
 </body>
 </html>
 <?php $this->endPage();
